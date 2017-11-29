ec38f5202936ca61eaa0626b1f961d06d77494d3
Jackson Hayes <asecretgate@gmail.com>
2017-11-29 23:39:28 +0000

    Working GET request using GitHub!

--- js/index.js
+++ js/index.js
@@ -1,20 +1,19 @@
+var userName = "jacksonhvisuals";
+var repoName = "5217-web";
+
 var change = document.getElementById("logContainer");
 var changeRequest = new XMLHttpRequest();
 
-changeRequest.open("GET", "json/changelog.json", true);
+changeRequest.open("GET", "https://api.github.com/repos/"+ userName +"/"+ repoName +"/releases", true);
 changeRequest.onreadystatechange = function() {
   if (changeRequest.readyState === 4) {
     if (changeRequest.status === 200 || changeRequest.status == 0) {
       var html = "";
 
       JSON.parse(changeRequest.responseText).forEach(function(change) {
-        var litem = "";
         console.log(change.name);
-        for (var i in change.description) {
-            litem += "<li> - " + change.description[i] + "</li><br>";
-        }
         var newName = change.name.replace(/\./g, "_");
-        html = "<div id=" + newName + " class=\"card\"><div class=\"card-content\"><span class=\"card-title\">" + change.name + "</span><div class=\"right reldate\">[" + change.reltype + "] " + change.reldate + "</div><ul>" +  litem + "</div></div>" + html;
+        html += "<div id=" + newName + " class=\"card\"><div class=\"card-content\"><span class=\"card-title\">" + change.name + "</span><div class=\"right reldate\">[" + change.prerelease + "] " + change.published_at + "</div><ul>" +  change.body + "</div></div>";
 
       });
       change.innerHTML = html;
