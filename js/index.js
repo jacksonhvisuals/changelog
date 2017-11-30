f65bc31414b5da3274252eeca2f7efe24b8f07df
Jackson Hayes <asecretgate@gmail.com>
2017-11-30 00:25:03 +0000

    Added pre-release if-loop check & loading message

--- js/index.js
+++ js/index.js
@@ -9,11 +9,15 @@ changeRequest.onreadystatechange = function() {
   if (changeRequest.readyState === 4) {
     if (changeRequest.status === 200 || changeRequest.status == 0) {
       var html = "";
-
+      var releaseType = "";
+      
       JSON.parse(changeRequest.responseText).forEach(function(change) {
+        if (change.prerelease === true) {
+          releaseType = "Pre-Release";
+        }
         console.log(change.name);
         var newName = change.name.replace(/\./g, "_");
-        html += "<div id=" + newName + " class=\"card\"><div class=\"card-content\"><span class=\"card-title\">" + change.name + "</span><div class=\"right reldate\">[" + change.prerelease + "] " + change.published_at + "</div><ul>" +  change.body + "</div></div>";
+        html += "<div id=" + newName + " class=\"card\"><div class=\"card-content\"><span class=\"card-title\">" + change.name + "</span><div class=\"right reldate\">[" + releaseType + "] " + change.published_at + "</div><ul>" +  change.body + "</div></div>";
 
       });
       change.innerHTML = html;
